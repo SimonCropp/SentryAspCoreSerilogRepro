2d70205ad8963661194d4a53e757cdb1281375af
Simon Cropp <simon.cropp@gmail.com>
2022-05-26 14:15:58 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore;
 using Serilog;
+using Serilog.Context;
 using Serilog.Events;
+using Serilog.Formatting.Display;
 
 namespace Sentry.Samples.AspNetCore.Serilog;
 
@@ -19,6 +21,7 @@ public class Program
                     {
                         s.MinimumBreadcrumbLevel = LogEventLevel.Debug;
                         s.MinimumEventLevel = LogEventLevel.Error;
+                        s.TextFormatter = new MessageTemplateTextFormatter("[{CorrelationId}] {Message}", null);
                     }))
 
             .UseSentry(_=>_.Dsn ="https://e8d57cabda394366b25b57bba7c204a6@o1027677.ingest.sentry.io/5994502")
@@ -27,9 +30,12 @@ public class Program
             {
                 a.Use(async (context, next) =>
                 {
-                    Log.Logger.Error(DateTime.Now.ToString());
+                    using (LogContext.PushProperty("CorrelationId", "myCorrelationId"))
+                    {
+                        Log.Logger.Error(DateTime.Now.ToString());
 
-                    await next();
+                        await next();
+                    }
                 });
             })
             .Build();
