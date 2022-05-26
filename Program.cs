42b61c1a77934c3d73e4be4a1abbb8faf254f71c
Simon Cropp <simon.cropp@gmail.com>
2022-05-26 12:47:01 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,32 +25,9 @@ public class Program
 
             .Configure(a =>
             {
-                // An example ASP.NET Core middleware that throws an
-                // exception when serving a request to path: /throw
                 a.Use(async (context, next) =>
                 {
-                    // See MinimumBreadcrumbLevel set at the Serilog configuration above
-                    Log.Logger.Debug("Static Serilog logger debug log stored as breadcrumbs.");
-
-                    var log = context.RequestServices.GetService<ILoggerFactory>()
-                        .CreateLogger<Program>();
-
-                    log.LogInformation("Handling some request...");
-
-                    // Sends an event which includes the info and debug messages above
-                    Log.Logger.Error("Logging using static Serilog directly also goes to Sentry.");
-
-                    var hub = context.RequestServices.GetService<IHub>();
-                    hub.ConfigureScope(s =>
-                    {
-                        // More data can be added to the scope like this:
-                        s.SetTag("Sample", "ASP.NET Core"); // indexed by Sentry
-                        s.SetExtra("Extra!", "Some extra information");
-                    });
-
-                    // Logging through the ASP.NET Core `ILogger` while using Serilog
-                    log.LogInformation("Logging info...");
-                    log.LogWarning("Logging some warning!");
+                    Log.Logger.Error(DateTime.Now.ToString());
 
                     await next();
                 });
